4b311da31722df3bde381918a21efadd3c3d9c05
LamerVano <vania_piskunov@tut.by>
2022-08-23 00:52:59 +0300

    add copy

--- background.js
+++ background.js
@@ -3,6 +3,32 @@ String.prototype.replaceAt = function(index, replacement)
   return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
+function copyTextToClipboard(text) {
+  //Create a textbox field where we can insert text to. 
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
+}
+
 var wordExtern = "";
 	
 translateText = function(info)
@@ -21,6 +47,8 @@ translateText = function(info)
 		else
 			wordExtern += ruChar[engChar.indexOf(word[i].toLowerCase())];
 	}
+	
+	copyTextToClipboard(wordExtern);
 	
 	chrome.notifications.create(
 		{
